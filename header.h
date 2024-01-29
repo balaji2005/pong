a7fc72a0ccef4104a1845501d27239545141502f
balaji2005 <balajikaredla12345@gmail.com>
2024-01-29 15:12:02 +0530

    Idk

--- header.h
+++ header.h
@@ -22,41 +22,104 @@ vector<Polygon> number(double x, double y) {
     return blocks;
 }
 
-// void display(double x, double y, int number) {
-//     vector<Polygon> digit = number(x, y);
-//     switch(number) {
-//         case 0:
-//             Polygon block = digit[0];
-//             block.setFill(true);
-//             block.setColor(COLOR("white"));
-//             block.imprint();
+void display(double x, double y, int number) {
+    double hor_positions[6][2] = {{-5*scale, 0}, {-4*scale, scale}, {4*scale, scale}, {5*scale, 0}, {4*scale, -scale}, {-4*scale, -scale}};
+    double ver_positions[6][2] = {{0, 5*scale}, {scale, 4*scale}, {scale, -4*scale}, {0, -5*scale}, {-scale, -4*scale}, {-scale, 4*scale}};
 
-//             for (int i=2; i<=6; i++) {
-//                 block = digit[i];
-//                 block.setFill(true);
-//                 block.setColor(COLOR("white"));
-//                 block.imprint();
-//             }
-//             break;
-//         case 1:
-//             block = digit[4];
-//             block.setFill(true);
-//             block.setColor(COLOR("white"));
-//             block.imprint();
+    Polygon block_1(x, y - 10*scale - 2, hor_positions, 6);
+    Polygon block_2(x, y, hor_positions, 6);
+    Polygon block_3(x, y + 10*scale + 2, hor_positions, 6);
+    Polygon block_4(x - 5*scale - 1, y - 5*scale - 1, ver_positions, 6);
+    Polygon block_5(x + 5*scale + 1, y - 5*scale - 1, ver_positions, 6);
+    Polygon block_6(x - 5*scale - 1, y + 5*scale + 1, ver_positions, 6);
+    Polygon block_7(x + 5*scale + 1, y + 5*scale + 1, ver_positions, 6);
 
-//             block = digit[6];
-//             block.setFill(true);
-//             block.setColor(COLOR("white"));
-//             block.imprint();
-//             break;
-//         default:
-//             for(Polygon block: digit) {
-//                 block.setFill(true);
-//                 block.setColor(COLOR("white"));
-//                 block.imprint();
-//             }
-//     }
-// }
+    block_1.setFill(true);
+    block_1.setColor(COLOR("white"));
+    block_2.setFill(true);
+    block_2.setColor(COLOR("white"));
+    block_3.setFill(true);
+    block_3.setColor(COLOR("white"));
+    block_4.setFill(true);
+    block_4.setColor(COLOR("white"));
+    block_5.setFill(true);
+    block_5.setColor(COLOR("white"));
+    block_6.setFill(true);
+    block_6.setColor(COLOR("white"));
+    block_7.setFill(true);
+    block_7.setColor(COLOR("white"));
+
+    switch(number) {
+        case 0:
+            block_1.imprint();
+            block_3.imprint();
+            block_4.imprint();
+            block_5.imprint();
+            block_6.imprint();
+            block_7.imprint();
+            break;
+        case 1:
+            block_5.imprint();
+            block_7.imprint();
+            break;
+        case 2:
+            block_1.imprint();
+            block_2.imprint();
+            block_3.imprint();
+            block_5.imprint();
+            block_6.imprint();
+            break;
+        case 3:
+            block_1.imprint();
+            block_2.imprint();
+            block_3.imprint();
+            block_5.imprint();
+            block_7.imprint();
+            break;
+        case 4:
+            block_2.imprint();
+            block_4.imprint();
+            block_5.imprint();
+            block_7.imprint();
+            break;
+        case 5:
+            block_1.imprint();
+            block_2.imprint();
+            block_3.imprint();
+            block_4.imprint();
+            block_7.imprint();
+            break;
+        case 6:
+            block_1.imprint();
+            block_2.imprint();
+            block_3.imprint();
+            block_4.imprint();
+            block_6.imprint();
+            block_7.imprint();
+            break;
+        case 7:
+            block_1.imprint();
+            block_5.imprint();
+            block_7.imprint();
+            break;
+        case 9:
+            block_1.imprint();
+            block_2.imprint();
+            block_3.imprint();
+            block_4.imprint();
+            block_5.imprint();
+            block_7.imprint();
+            break;
+        default:
+            block_1.imprint();
+            block_2.imprint();
+            block_3.imprint();
+            block_4.imprint();
+            block_5.imprint();
+            block_6.imprint();
+            block_7.imprint();
+    }
+}
 
 // Function to create game outline
 void reset() {
@@ -94,12 +157,13 @@ void reset() {
     // block.setColor(COLOR("white"));
     // block.imprint();
 
-    vector<Polygon> digit = number(400, 300);
-    for (Polygon block: digit) {
-        block.setFill(true);
-        block.setColor(COLOR("white"));
-        block.imprint();
-    }
+    // vector<Polygon> digit = number(400, 300);
+    // for (Polygon block: digit) {
+    //     block.setFill(true);
+    //     block.setColor(COLOR("white"));
+    //     block.imprint();
+    // }
+    display(400, 300, 9);
     // display(400, 300, 0);
     wait(5);
 }
